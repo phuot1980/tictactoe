b8306ee68e13fdd4aef1ba23a3fd5e630fbdb117
ponlok huot <phuot1980@gmail.com>
2014-12-10 22:13:24 -0800

    added currentplayer to be able to switch between x and o

--- js/main.js
+++ js/main.js
@@ -4,36 +4,38 @@ $(document).ready(tictactoe)
 function tictactoe()
 {
 	var gameBoard = new Array(9);
-	
+	var currentPlayer = 1;
+
 	//for-in loop to name the #box and add anEventListener to each div
 	for (var i = 0; i<9; i++)
 	{
 		$('#box' + i).click(markBox);
-		// console.log("#box"+i);
+		console.log("#box"+i);
 	}
-
 
 	function markBox()
 	{
-		//gets div information
+		//gets div information and returns number
 		var playerMove = $(this).attr('id').slice(-1);
 
 		//set a player's move in the gameBoard
-		gameBoard[playerMove] = 'x';
+		// gameBoard[playerMove] = 'x';
+
+		if (currentPlayer == 1) 
+		{
+			gameBoard[playerMove] = 'x';
+			currentPlayer = 2;
+		}
+		else 
+		{
+			gameBoard[playerMove] = 'O';
+			currentPlayer = 1;
+		}
+
+
 
 		console.log(gameBoard)
 	}
-
-
-	function switchPlayer()
-	{
-
-	}
-
-
-
-
-
 
 	return;
 };
