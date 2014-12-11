32063148ff2fcc72538d507ec6c3dd6cd9d96de5
ponlok huot <phuot1980@gmail.com>
2014-12-10 23:08:57 -0800

    added win conditions

--- js/main.js
+++ js/main.js
@@ -13,14 +13,14 @@ function tictactoe()
 		console.log("#box"+i);
 	}
 
+	//tracking player movement 
 	function markBox()
 	{
 		//gets div information and returns number
 		var playerMove = $(this).attr('id').slice(-1);
 
-		//set a player's move in the gameBoard
-		// gameBoard[playerMove] = 'x';
-
+	
+		//change player from x to o
 		if (currentPlayer == 1) 
 		{
 			gameBoard[playerMove] = 'x';
@@ -32,13 +32,61 @@ function tictactoe()
 			currentPlayer = 1;
 		}
 
-
+		checkWinCondition()
 
 		console.log(gameBoard)
 	}
+	//check win condition in rows, column and diagnols
+	function checkWinCondition()
+	{
+				// check every single row
+		if (((gameBoard[0] == gameBoard[1]) && (gameBoard[1] == gameBoard[2])) && 
+			((gameBoard[0] != undefined) || (gameBoard[1] != undefined) || (gameBoard[2] != undefined)))
+		{
+			console.log('winner');
+		}
+		else if (((gameBoard[3] == gameBoard[4]) && (gameBoard[4] == gameBoard[5])) && 
+			((gameBoard[3] != undefined) || (gameBoard[4] != undefined) || (gameBoard[5] != undefined)))
+		{
+			console.log('winner');
+		}
+		else if (((gameBoard[6] == gameBoard[7]) && (gameBoard[7] == gameBoard[8])) && 
+			((gameBoard[6] != undefined) || (gameBoard[7] != undefined) || (gameBoard[8] != undefined)))
+		{
+			console.log('winner');
+		}
+		// check every column
+		else if (((gameBoard[0] == gameBoard[3]) && (gameBoard[3] == gameBoard[6])) && 
+			((gameBoard[0] != undefined) || (gameBoard[3] != undefined) || (gameBoard[6] != undefined)))
+		{
+			console.log('winner');
+		}
+		else if (((gameBoard[1] == gameBoard[4]) && (gameBoard[4] == gameBoard[7])) && 
+			((gameBoard[1] != undefined) || (gameBoard[4] != undefined) || (gameBoard[7] != undefined)))
+		{
+			console.log('winner');
+		}
+		else if (((gameBoard[2] == gameBoard[5]) && (gameBoard[5] == gameBoard[8])) && 
+			((gameBoard[2] != undefined) || (gameBoard[5] != undefined) || (gameBoard[8] != undefined)))
+		{
+			console.log('winner');
+		}
+		//check diagnols
+		else if (((gameBoard[0] == gameBoard[4]) && (gameBoard[4] == gameBoard[8])) && 
+			((gameBoard[0] != undefined) || (gameBoard[4] != undefined) || (gameBoard[8] != undefined)))
+		{
+			console.log('winner');
+		}
+		else if (((gameBoard[2] == gameBoard[4]) && (gameBoard[4] == gameBoard[6])) && 
+			((gameBoard[2] != undefined) || (gameBoard[4] != undefined) || (gameBoard[6] != undefined)))
+		{
+			console.log('winner');
+		}
 
+	}// function checkWinCOndition
 	return;
 };
+
 
 
 
