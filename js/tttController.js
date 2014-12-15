5ca66761ac3c8a138e034dccf6cc913dbc29282e
ponlok huot <phuot1980@gmail.com>
2014-12-14 23:00:54 -0800

    updated function() return true false

--- js/tttController.js
+++ js/tttController.js
@@ -27,20 +27,41 @@ angular
 			//this is player 1
 			if (self.gameSession.currentPlayer == 1) 
 			{
-				self.gameSession.gameBoard[i] = 'x';
-				self.gameSession.currentPlayer = 2 ;
-				console.log(self.gameSession.gameBoard);
+				//i can only put an x if the box is not occupied. 
+				if (self.gameSession.gameBoard[i] == "")
+				{
+					self.gameSession.gameBoard[i] = 'x';
+					self.gameSession.currentPlayer = 2 ;
+					console.log(self.gameSession.gameBoard);
+				}
+				
 			}
 			//this player 2
 			else
 			{
-				self.gameSession.gameBoard[i] = 'o';
-				self.gameSession.currentPlayer = 1;
-				console.log(self.gameSession.gameBoard);
+				//i can only put an o if the box is not occupied. 
+				if (self.gameSession.gameBoard[i] == "")
+				{
+					self.gameSession.gameBoard[i] = 'o';
+					self.gameSession.currentPlayer = 1;
+					console.log(self.gameSession.gameBoard);
+				}
 			}
+			// checkTie runs when there is no winner. 
+			// if there is a winner then there can be no tie. 
+			if (checkWin() == false)
+			{
 
-			checkWin();
-			checkTie();
+				if (checkTie() == true)
+				{
+					console.log('tie');
+				}
+			}
+			else
+			{
+				//display a winner
+				console.log('winner');
+			}
 			// saving player move into firebase. 
 			self.gameSession.$save();
 		}
@@ -71,23 +92,36 @@ angular
 			if (((getTile(x) == getTile(y)) && (getTile(y) == getTile(z))) && 
 				((getTile(x) != "") || (getTile(y) != "") || (getTile(z) != "")))
 			{
-				console.log("winner")
+				
+				// console.log("winner");
+				//winner found
+				return true;
 			}
+			//no winner found
+			return false;
+
 		}
 
 		function checkWin()
 		{
+			if (
 			//check horiztontal rows
-			checkRow(0,1,2);
-			checkRow(3,4,5);
-			checkRow(6,7,8);
+			(checkRow(0,1,2)) ||
+			(checkRow(3,4,5)) ||
+			(checkRow(6,7,8)) ||
 			//check vertical rows
-			checkRow(0,3,6);
-			checkRow(1,4,7);
-			checkRow(2,5,8);
+			(checkRow(0,3,6)) ||
+			(checkRow(1,4,7)) ||
+			(checkRow(2,5,8)) ||
 			//check diagonals
-			checkRow(0,4,8);
-			checkRow(2,4,6);
+			(checkRow(0,4,8)) ||
+			(checkRow(2,4,6)))
+			{
+				//winner is found
+				return true;
+			}
+			//winner is not found 
+			return false;
 		}
 
 		function checkTie()
@@ -96,13 +130,13 @@ angular
 			{
 				if (self.getTile(i) == "")
 				{
-					console.log('not a tie')
+					// console.log('not a tie')
 					return false;
 					//not a tie	
 				}
 			}
 			//tie 
-			console.log('this is a tie')
+			// console.log('this is a tie')
 			return true;
 		}
 	}
